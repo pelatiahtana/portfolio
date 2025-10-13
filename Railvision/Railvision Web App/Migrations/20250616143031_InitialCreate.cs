1e8f7eb00d1826a3f30fbbcb7f7c26d44f7e87c9
pelatiahtana <135526117+pelatiahtana@users.noreply.github.com>
2025-10-13 15:00:47 +0200

    Add files via upload

---
+++ Railvision/Railvision Web App/Migrations/20250616143031_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrainGenie.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WagonDetachmentLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Time = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    WagonId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WagonDetachmentLogs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WagonDetachmentLogs");
+        }
+    }
+}
---
+++ Railvision/Railvision Web App/Migrations/20250622181950_UpdateWagonDetachmentLog.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrainGenie.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateWagonDetachmentLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Actions",
+                table: "WagonDetachmentLogs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "WagonDetachmentLogs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReportedBy",
+                table: "WagonDetachmentLogs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Severity",
+                table: "WagonDetachmentLogs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "WagonDetachmentLogs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TrainId",
+                table: "WagonDetachmentLogs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Actions",
+                table: "WagonDetachmentLogs");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "WagonDetachmentLogs");
+
+            migrationBuilder.DropColumn(
+                name: "ReportedBy",
+                table: "WagonDetachmentLogs");
+
+            migrationBuilder.DropColumn(
+                name: "Severity",
+                table: "WagonDetachmentLogs");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "WagonDetachmentLogs");
+
+            migrationBuilder.DropColumn(
+                name: "TrainId",
+                table: "WagonDetachmentLogs");
+        }
+    }
+}
