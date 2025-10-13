0eb0ca3190a0079a663b4f7894788946b3245efd
pelatiahtana <135526117+pelatiahtana@users.noreply.github.com>
2025-10-13 14:56:45 +0200

    Add files via upload

---
+++ Railvision/Railvision Web App/Hubs/IncidentHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using TrainGenie.Models;
+
+namespace TrainGenie.Hubs
+{
+    public class IncidentHub : Hub
+    {
+        public async Task NotifyNewIncident(Incident incident)
+        {
+            await Clients.Group("Admins").SendAsync("ReceiveNewIncident", incident);
+        }
+
+        public async Task NotifyIncidentUpdate(Incident incident)
+        {
+            await Clients.Group("Admins").SendAsync("ReceiveIncidentUpdate", incident);
+        }
+
+        public async Task JoinAdminGroup()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "Admins");
+        }
+    }
+}
